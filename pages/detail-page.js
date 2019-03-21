48dc20d71a399cb620ad01887760721c560948e8
Skiffen <41432325+Skiffen@users.noreply.github.com>
2019-03-21 13:09:01 +0100

    Set Up Form Validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,12 +1,42 @@
+const doesNotPassAllValidations = (name, msg) => {
+    if (!name && !msg) {
+        alert('You forgot to fill in your name or message!');
+        return true;
+    } else if (!name) {
+        alert('You forgot to fill in your name!');
+        return true;
+    } else if (!msg) {
+        alert('You forgot to fill in your message!');
+        return true;
+    } else if (msg.length > 280) {
+        alert('Your comment is too long!');
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const nameDoesNotStartWithUppercase = name => {
+    if (name[0].toLowerCase() === name[0]) {
+        return name[0].toUpperCase() + name.slice(1);
+    } else {
+    return name
+    }
+}
 const submitComment = () => {
     const inputField = document.getElementById('name');
-    const name = inputField.value;
+    let name = inputField.value;
     const textArea = document.getElementById('msg');
     const msg = textArea.value;
     
     const comment = document.createElement('section');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
+
+    if (doesNotPassAllValidations(name, msg) === true) {
+        return null;
+    }
+    name = nameDoesNotStartWithUppercase(name);
 
     h3.innerHTML = `${name} said:`;
     p.innerHTML = msg;
